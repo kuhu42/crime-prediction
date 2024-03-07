01bfd5c681ab5522b15ba3a6fb67b22dbf70bd8e
Anushka Srivastava <anusri1904@gmail.com>
2024-03-08 02:04:37 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import user_login, home
 urlpatterns = [
 path('admin/', admin.site.urls),
 path('', include('loginanalysis.urls')),
+path('login/', user_login, name='login'),
+path('home/', home, name='home'),
 ]
